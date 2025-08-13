55103fc9334b4a052222eda3f63fafc007605715
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2025-08-13 19:33:52 +0800

    feat: Add prompts and localize variables

--- dldw.sh
+++ dldw.sh
@@ -11,15 +11,21 @@ function login {
     exit 1
   fi
 
-  TOKEN=$(awk '/XSRF-TOKEN/ {print $7}' dlsite-cookie.txt)
-  FORM_DATA="_token=$TOKEN&login_id=$USERNAME&password=$PASSWORD"
+  local TOKEN=$(awk '/XSRF-TOKEN/ {print $7}' dlsite-cookie.txt)
+  local FORM_DATA="_token=$TOKEN&login_id=$USERNAME&password=$PASSWORD"
+  local CONTENT=$(curl -s -b dlsite-cookie.txt -c dlsite-cookie.txt -d "$FORM_DATA" -H "Content-Type: application/x-www-form-urlencoded" -L https://login.dlsite.com/login)
 
-  curl -s -b dlsite-cookie.txt -c dlsite-cookie.txt -d "$FORM_DATA" -H "Content-Type: application/x-www-form-urlencoded" -L -o /dev/null https://login.dlsite.com/login
+  if [[ $CONTENT != *"ログインが完了しました。"* ]]; then
+    echo "Login failed."
+    exit 1
+  else
+    echo "Login success."
+  fi
 }
 
 function product {
-  PRODUCT_SUBCOMMAND=${POSITIONAL_ARGS[0]}
-  POSITIONAL_ARGS=("${POSITIONAL_ARGS[@]:1}")
+  local PRODUCT_SUBCOMMAND=${POSITIONAL_ARGS[0]}
+  local POSITIONAL_ARGS=("${POSITIONAL_ARGS[@]:1}")
 
   case "$PRODUCT_SUBCOMMAND" in
   list)
@@ -32,15 +38,15 @@ function product {
 }
 
 function product_list {
-  USER_PRODUCT_COUNT=$(curl -s -b dlsite-cookie.txt https://play.dlsite.com/api/product_count | jq '.user')
-  COUNT=1
-  PRODUCT_LIST=
+  local USER_PRODUCT_COUNT=$(curl -s -b dlsite-cookie.txt https://play.dlsite.com/api/product_count | jq '.user')
+  local COUNT=1
+  local PRODUCT_LIST=
 
   while ((USER_PRODUCT_COUNT > 0)); do
-    PRODUCT_WORKS=$(curl -s -b dlsite-cookie.txt "https://play.dlsite.com/api/purchases?page=$COUNT" | jq '.works')
-    PRODUCT_LIST="$PRODUCT_LIST$PRODUCT_WORKS"
+    local PRODUCT_WORKS=$(curl -s -b dlsite-cookie.txt "https://play.dlsite.com/api/purchases?page=$COUNT" | jq '.works')
+    local PRODUCT_LIST="$PRODUCT_LIST$PRODUCT_WORKS"
     ((COUNT++))
-    USER_PRODUCT_COUNT=$(($USER_PRODUCT_COUNT - 50))
+    local USER_PRODUCT_COUNT=$(($USER_PRODUCT_COUNT - 50))
   done
 
   PRODUCT_LIST=$(echo $PRODUCT_LIST | jq -s 'add')
@@ -54,24 +60,24 @@ function product_list {
 }
 
 function product_files {
-  WORKNO=$1
-  FILES_COUNT=$(curl -s https://www.dlsite.com/maniax/api/=/product.json?workno=$WORKNO | jq '.[].contents | length')
-  OUTPUT_DIR=${OUTPUT_DIR:-./downloads}
+  local WORKNO=$1
+  local FILES_COUNT=$(curl -s https://www.dlsite.com/maniax/api/=/product.json?workno=$WORKNO | jq '.[].contents | length')
+  local OUTPUT_DIR=${OUTPUT_DIR:-./downloads}
 
   case $FILES_COUNT in
   0) ;;
   1)
-    HEADER=$(curl -s -b dlsite-cookie.txt -o /dev/null -D - "https://www.dlsite.com/home/download/=/product_id/$WORKNO.html")
-    DOWNLOAD_URL=$(echo "$HEADER" | grep -i "^Location" | awk '{print $2}' | tr -d '\r')
-    JWT=$(echo "$HEADER" | grep -i "^Set-Cookie: " | awk -F': ' '{print $2}' | tr -d '\r')
+    local HEADER=$(curl -s -b dlsite-cookie.txt -o /dev/null -D - "https://www.dlsite.com/home/download/=/product_id/$WORKNO.html")
+    local DOWNLOAD_URL=$(echo "$HEADER" | grep -i "^Location" | awk '{print $2}' | tr -d '\r')
+    local JWT=$(echo "$HEADER" | grep -i "^Set-Cookie: " | awk -F': ' '{print $2}' | tr -d '\r')
 
     aria2c -x 10 -s 10 --header="Cookie: $JWT" -d "$OUTPUT_DIR" $DOWNLOAD_URL
     ;;
   *)
     for i in $(seq 1 $FILES_COUNT); do
-      HEADER=$(curl -s -b dlsite-cookie.txt -o /dev/null -D - "https://www.dlsite.com/home/download/=/number/$i/product_id/$WORKNO.html")
-      DOWNLOAD_URL=$(echo "$HEADER" | grep -i "^Location" | awk '{print $2}' | tr -d '\r')
-      JWT=$(echo "$HEADER" | grep -i "^Set-Cookie: " | awk -F': ' '{print $2}' | tr -d '\r')
+      local HEADER=$(curl -s -b dlsite-cookie.txt -o /dev/null -D - "https://www.dlsite.com/home/download/=/number/$i/product_id/$WORKNO.html")
+      local DOWNLOAD_URL=$(echo "$HEADER" | grep -i "^Location" | awk '{print $2}' | tr -d '\r')
+      local JWT=$(echo "$HEADER" | grep -i "^Set-Cookie: " | awk -F': ' '{print $2}' | tr -d '\r')
 
       aria2c -x 10 -s 10 --header="Cookie: $JWT" -d "$OUTPUT_DIR" $DOWNLOAD_URL
     done
